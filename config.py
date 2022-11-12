d7b663b74fceb67be041ea502985a5f4a609657d
Ramon Marquez <rmblockcode@gmail.com>
2022-11-11 23:05:45 -0500

    sqlalchemy variable

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 ETHERSCAN_API_KEY = os.getenv('ETHERSCAN_API_KEY')
 ETHERSCAN_API_URL = os.getenv('ETHERSCAN_API_URL')
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL').replace("postgres://", "postgresql://", 1)
 COINGECKO_API = os.getenv('COINGECKO_API')
